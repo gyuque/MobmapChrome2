147444aca18228542dc25b22cb37f16beec43c15
上山 智士 <gyuque@gmail.com>
2013-11-11 16:18:29 +0900

    column detector test

--- inner/js/ui/csv-preview-window.js
+++ inner/js/ui/csv-preview-window.js
@@ -199,7 +199,100 @@ if (!window.mobmap) { window.mobmap={}; }
 			}
 		}
 	};
+	
+	
+	function ColumnsAutoDetector() {
+		this.success = false;
+		this.colmap = {};
+	}
+	
+	ColumnsAutoDetector.RE_ID = /^[0-9 \t]+$/
+	ColumnsAutoDetector.RE_COORD = /^[-+.0-9 \t]+$/
+	
+	ColumnsAutoDetector.prototype = {
+		detect: function(records) {
+			var targetRow = records[1] || records[0];
+			if (!targetRow) {
+				return;
+			}
+			
+			this.detectId(targetRow);
+			this.success = true;
+		},
+		
+		detectId: function(targetRow) {
+			this.findColumnByCondition(
+				targetRow,
+				'id',
+				function(field) {
+					return ColumnsAutoDetector.RE_ID.test(field);
+				}
+			);
+		},
+		
+		findColumnByCondition: function(targetRow, attr_name, cond_func) {
+			var len = targetRow.length;
+			for (var i = 0;i < len;++i) {
+				if (this.colmap[i]) {continue;}
+				
+				if (cond_func(targetRow[i])) {
+					this.colmap[i] = attr_name;
+					return i;
+				}
+			}
+			
+			return -1;
+		},
+		
+		getIdColumnIndex: function()   { return this.getColumnIndexByName('id'); },
+		getTimeColumnIndex: function() { return this.getColumnIndexByName('time'); },
+		getXColumnIndex: function()    { return this.getColumnIndexByName('x'); },
+		getYColumnIndex: function()    { return this.getColumnIndexByName('y'); },
+		
+		getColumnIndexByName: function(name) {
+			for (var i in this.colmap) {
+				if (this.colmap[i] === name) {
+					return (i-0);
+				}
+			}
+			
+			return -1;
+		}
+	};
 
+	// / / test data
+	var TestRecords = [
+	 ["01972939","2013-02-24 00:03:43","35.658048","139.641874"],["00690961","2013-02-24 00:10:26","35.677768","139.665263"],
+	 ["00529402","2013-02-24 00:24:10","35.685932","139.701483"],["01733612","2013-02-24 00:28:02","35.583333","139.674425"],
+	 ["00529402","2013-02-24 00:29:02","35.67971","139.721653"],["00919054","2013-02-24 00:32:42","35.642588","139.620824"],
+	 ["00529402","2013-02-24 00:33:58","35.701779","139.744828"],["00529402","2013-02-24 00:39:00","35.699944","139.764869"],
+	 ["00529402","2013-02-24 00:44:02","35.698603","139.772487"],["01142511","2013-02-24 00:45:07","35.64718","139.838362"]];
+
+	function testColumnsAutoDetector() {
+		var detector = new ColumnsAutoDetector();
+		detector.detect(TestRecords);
+		
+		if (!detector.success) {
+			throw "detector must success";
+		}
+		
+		var idcol = detector.getIdColumnIndex();
+		if (idcol !== 0) {
+			throw "id column must be 0";
+		}
+
+		var ycol = detector.getYColumnIndex();
+		if (ycol !== 2) {
+			throw "y column must be 2";
+		}
+
+		var tcol = detector.getTimeColumnIndex();
+		if (tcol !== 1) {
+			throw "time column must be 1";
+		}
+	}
+
+	testColumnsAutoDetector();
 	// +++ Export +++
 	aGlobal.mobmap.CSVPreviewWindow = CSVPreviewWindow;
 })(window);
