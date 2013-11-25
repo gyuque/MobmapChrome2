bd1068c342410b6873a52d481ae8a7b0b8f031ae
上山 智士 <gyuque@gmail.com>
2013-11-25 16:49:10 +0900

    gl layer resizing

--- js/gl-test.js
+++ js/gl-test.js
@@ -2,19 +2,33 @@
 	'use strict';
 
 	var gmap = null;
+	var gllayer = null;
 	var mapContainerElement = null;
 	
 	function doGLMapTest() {
-		
+		setupScreen();
 	}
 	
 	function setupScreen() {
 		var box = document.getElementById("map-box");
 		mapContainerElement = box;
+		
+		setupGoogleMaps(box);
 	}
 	
 	function setupGoogleMaps(containerElement) {
+		var mapOptions = {
+			zoom: 11,
+			center: new google.maps.LatLng(35.8, 139.6),
+			mapTypeId: google.maps.MapTypeId.ROADMAP
+		};
+
+		gmap = new google.maps.Map(containerElement, mapOptions);
 		
+		// ----------------------------------------------
+		
+		gllayer = new mobmap.GLMobLayer();
+		gllayer.setMap(gmap);
 	}
 
 	aGlobal.doGLMapTest = doGLMapTest;
