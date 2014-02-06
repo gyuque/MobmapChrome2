14b7e52d4d12a21f1cdfe2e1cfb568ea7ee3e7b3
上山 智士 <gyuque@gmail.com>
2014-02-06 17:07:11 +0900

    render map

--- js/jd-main.js
+++ js/jd-main.js
@@ -15,27 +15,46 @@
 
 
 	function PrefPicker(map_container_el) {
-		this.mapWidth  = 400;
-		this.mapHeight = 300;
+		this.mapWidth  = 320;
+		this.mapHeight = 360;
 		
+		this.mapOffsetX = -440;
+		this.mapOffsetY = -70;
+		
+		this.mapCanvas  = null;
+		this.mapG = null;
 		this.mapContainerElement = map_container_el;
 		this.onImageReadyCallback = null;
 		this.prefNameMap = {}
+		this.buildView();
 	}
 	
 	PrefPicker.prototype = {
+		buildView: function() {
+			this.mapCanvas = document.createElement('canvas');
+			this.mapG = this.mapCanvas.getContext('2d');
+			
+			this.mapCanvas.width = this.mapWidth;
+			this.mapCanvas.height = this.mapHeight;
+			this.mapContainerElement.appendChild(this.mapCanvas);
+		},
+
 		setMapSource: function(sourceData, callback) {
 			this.onImageReadyCallback = callback;
 			for (var nm in sourceData) {
 				var dat = sourceData[nm];
-				this.registerPrefecture(nm, dat.data);
+				this.registerPrefecture(nm, dat.data, dat.originX, dat.originY);
 			}
 			
 			this.loadAll();
 		},
 
-		registerPrefecture: function(name, imageURL) {
+		registerPrefecture: function(name, imageURL, ox, oy) {
 			var item = new PrefItem();
+			
+			item.originX = ox;
+			item.originY = oy;
+			
 			item.image = new Image();
 			item.image.onload = this.onItemImageLoadComplete.bind(this, item);
 			item.sourceURL = imageURL;
@@ -68,7 +87,19 @@
 		},
 		
 		render: function() {
+			var g = this.mapG;
+			g.clearRect(0, 0, this.mapWidth, this.mapHeight);
 			
+			for (var nm in this.prefNameMap) {
+				var item = this.prefNameMap[nm];
+				this.renderPrefImage(g, item);
+			}
+		},
+		
+		renderPrefImage: function(g, prefItem) {
+			g.drawImage(prefItem.image,
+				this.mapOffsetX + prefItem.originX,
+				this.mapOffsetY + prefItem.originY);
 		}
 	};
 	
@@ -76,7 +107,20 @@
 		this.image = null;
 		this.sourceURL = null;
 		this.loadComplete = false;
+		this.originX = 0;
+		this.originY = 0;
 	}
+	
+	
+	function SelectionSet() {
+		this.jEventElement = $('body');
+	}
+	
+	SelectionSet.prototype = {
+		eventDispatcher: function() {
+			return this.jEventElement;
+		}
+	};
 	
 	PrefItem.prototype.load = function() { this.image.src = this.sourceURL; };
 })(window);
