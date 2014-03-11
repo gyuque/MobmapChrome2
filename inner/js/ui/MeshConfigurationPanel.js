fa578f8b4b1ebcb17d1766c517183633fcc36edf
上山 智士 <gyuque@gmail.com>
2014-03-11 16:18:08 +0900

    add MeshConfigurationPanel object

---
+++ inner/js/ui/MeshConfigurationPanel.js
@@ -0,0 +1,32 @@
+if (!window.mobmap) { window.mobmap={}; }
+// [][][][] ||||||||||||||||||
+// [][][][] MESH CONFIGURATION
+// [][][][] ||||||||||||||||||
+
+(function(aGlobal) {
+	'use strict';
+	
+	function MeshConfigurationPanel(layer) {
+		this.boundLayer = layer;
+
+		this.expandablePanel = new mobmap.ExpandablePanel();
+		this.element = this.expandablePanel.element;
+		this.jElement = $(this.element);
+		this.expandablePanel.setTitle("Mesh");
+
+		this.configurePanelContent();
+	}
+	
+	MeshConfigurationPanel.prototype = {
+		configurePanelContent: function() {
+			var ec = this.expandablePanel.expandedContentElement;
+//			ec.innerHTML = '';
+//			ec.appendChild( this.markerGenerator.resultCanvas );
+		},
+		
+		show: function() { this.expandablePanel.show(); },
+		hide: function() { this.expandablePanel.hide(); }
+	};
+
+	aGlobal.mobmap.MeshConfigurationPanel = MeshConfigurationPanel;
+})(window);
