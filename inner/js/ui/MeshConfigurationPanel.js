21d2f36082bfc366325740bfc07016e0076dd5a5
上山 智士 <gyuque@gmail.com>
2014-03-12 17:30:18 +0900

    #37 detail controller

--- inner/js/ui/MeshConfigurationPanel.js
+++ inner/js/ui/MeshConfigurationPanel.js
@@ -12,6 +12,8 @@ if (!window.mobmap) { window.mobmap={}; }
 		this.expandablePanel = new mobmap.ExpandablePanel();
 		this.element = this.expandablePanel.element;
 		this.jElement = $(this.element);
+		this.jValRangeSlider = null;
+		this.jValRangeDispBox = null;
 		this.expandablePanel.setTitle("Mesh coloring");
 
 		this.gradientEditor = new mobmap.GradientEditor( layer.colorRule );
@@ -27,6 +29,27 @@ if (!window.mobmap) { window.mobmap={}; }
 			var ec = this.expandablePanel.expandedContentElement;
 			ec.innerHTML = '';
 			ec.appendChild( this.gradientEditor.element );
+			
+			this.buildValueRangeController(ec);
+		},
+		
+		buildValueRangeController: function(container) {
+			var box = $H('div', 'mm-gradient-val-range-control-box');
+			
+			var labelbox = $H('div');
+			labelbox.innerHTML = "Val. range:";
+			
+			var val_range = $H('input', 'mm-gradient-val-range-slider');
+			val_range.type = 'range';
+			val_range.min = 0;
+			val_range.max = mobmap.MMMeshLayer.RangePreset.length - 1;
+
+			this.jValRangeSlider = $(val_range);
+			this.jValRangeDispBox = $(labelbox);
+			
+			box.appendChild(labelbox);
+			box.appendChild(val_range);
+			container.appendChild(box);
 		},
 		
 		show: function() { this.expandablePanel.show(); },
