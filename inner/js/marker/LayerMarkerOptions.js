355a6f8fe1f101ac73d5b485d81b78bf6d6cd17f
上山 智士 <gyuque@gmail.com>
2014-07-23 17:48:17 +0900

    Index map parser

--- inner/js/marker/LayerMarkerOptions.js
+++ inner/js/marker/LayerMarkerOptions.js
@@ -94,6 +94,113 @@ if (!window.mobmap) { window.mobmap={}; }
 		}
 	};
 	
+	LayerMarkerOptions.CustomIndexMapping = function() {
+		this.rawToIndexMap = {};
+	};
+	
+	LayerMarkerOptions.CustomIndexMapping.prototype = {
+		parse: function(sourceText) {
+			var pair_list = LayerMarkerOptions.CustomIndexMapping.splitTokens(sourceText);
+			for (var i in pair_list) {
+				var pair = pair_list[i];
+				this.rawToIndexMap[ pair.lefthand.content ] = pair.righthand.content;
+			}
+		},
+		
+		dump: function() {
+			var m = this.rawToIndexMap;
+			console.log('------------------------------------');
+			for (var i in m) {
+				console.log(i + ' => ', m[i])
+			}
+			console.log('------------------------------------');
+		}
+	};
+	
+	
+	var RE_STRL = /^(('[^']*')|(\"[^\"]*\"))/ ;
+	var RE_WS   = /^\s+/ ;
+	var RE_DIGIT= /^([0-9]+)/ ;
+	var RE_DELIM= /^(\:)/ ;
+	
+	var kTokTypeDigit  = 0;
+	var kTokTypeStrLit = 1;
+	var kTokTypeDelim  = 2;
+	
+	var kExpectRawValue = 0;
+	var kExpectDelim    = 1;
+	var kExpectIndex    = 2;
+	
+	LayerMarkerOptions.CustomIndexMapping.dequote = function(s) {
+		return s.replace(/^['"]/, '').replace(/['"]$/, '');
+	};
+	
+	LayerMarkerOptions.CustomIndexMapping.splitTokens = function(sourceText) {
+		var exp = kExpectRawValue;
+		var results = [];
+		
+		var tempRawValToken = null;
+		
+		for (var i = 0;i < 999;++i) {
+			sourceText = sourceText.replace(RE_WS, '');
+
+			var foundToken = null;
+			if (RE_STRL.test(sourceText)) {
+				sourceText = sourceText.replace(RE_STRL, '');
+				foundToken = {
+					type: kTokTypeStrLit,
+					content: LayerMarkerOptions.CustomIndexMapping.dequote( RegExp['$1'] )
+				};
+			} else if (RE_DIGIT.test(sourceText)) {
+				sourceText = sourceText.replace(RE_DIGIT, '');
+				var matched_digit = RegExp['$1'];
+				foundToken = {
+					type: kTokTypeDigit,
+					content: parseInt(matched_digit, 10)
+				};
+			} else if (RE_DELIM.test(sourceText)) {
+				sourceText = sourceText.replace(RE_DELIM, '');
+				foundToken = {
+					type: kTokTypeDelim
+				};
+			}
+			
+			
+			switch(exp) {
+				case kExpectRawValue:
+					if (!foundToken) { return results; }
+					if (foundToken.type === kTokTypeDigit || foundToken.type === kTokTypeStrLit) {
+						tempRawValToken = foundToken;
+						++exp;
+					} else {
+						// Error
+						return null;
+					}
+					break;
+					
+				case kExpectDelim:
+					if (!foundToken) { return null; /*Error*/ }
+					if (foundToken.type !== kTokTypeDelim) { return null; /*Error*/ }
+					
+					++exp;
+					break;
+
+				case kExpectIndex:
+					if (!foundToken) { return null; /*Error*/ }
+					if (foundToken.type !== kTokTypeDigit) { return null; /*Error*/ }
+					
+					results.push({
+						lefthand: tempRawValToken,
+						righthand: foundToken
+					});
+					
+					exp = kExpectRawValue;
+					break;
+			}
+		}
+	};
+	
+	
 	LayerMarkerOptions.MV_NONE = 0;
 	LayerMarkerOptions.MV_ATTR = 1;
 	LayerMarkerOptions.MV_DAY  = 2;
